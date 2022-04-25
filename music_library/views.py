fadd97579c1a912ca6e3d995a918b4e514cec17c
Akeem <akeem.mighty@gmail.com>
2022-04-25 16:07:44 -0400

    Completed Get and Post Functionality For Music Library

--- music_library/views.py
+++ music_library/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializer import MusicLibrarySerializer
+from music_library.models import MusicLibrary
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def music_list(request):
 
-    
-    return Response('ok')
+    if request.method == 'GET':
+        music = MusicLibrary.objects.all()
+        serializer = MusicLibrarySerializer(music, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MusicLibrarySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
