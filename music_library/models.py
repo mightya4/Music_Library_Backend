dc576e81d7ee05771691484ff61e73a4caf946ca
Akeem <akeem.mighty@gmail.com>
2022-04-25 15:22:45 -0400

    Created Music Library Model for Music_Library App + Registered Products in Project Settings Installed Apps Section

--- music_library/models.py
+++ music_library/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class MusicLibrary(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=255)
+
+
